96d90fb7a24604eeb30832292f2882a13f1a208c
blackeduard <165398613+blackeduard@users.noreply.github.com>
2024-05-03 08:45:35 -0300

    crud - 03/05

--- 3° EMTIS/Gustavo/Python/aula2.py
+++ 3° EMTIS/Gustavo/Python/aula2.py
@@ -35,7 +35,33 @@ def deletar_usuario(id):
     cursor = conexao.cursor()
     cursor.execute('''DELETE FROM usuarios where id = ?''',(id,))
 
-# Função para atualizar os dados de um usuário
-def atualizar_usuario(id, nome, idade):
-    conexao = sqlite3.connect('aluno.db')
-    cursor = conexao.cursor()
+# Função do menu de escolhas
+def menu():
+    print("\n1. Adicionar usuário")
+    print("2. Listar usuário")
+    print("3. Atualizar usuário")
+    print("4. Deletar usuário")
+    print("5. Sair")
+
+# Criar a tabela
+while True:
+    menu()
+    escolha = input("Escolha a opção")
+
+    if escolha == '1':
+        nome = input("Digite o nome do usuário: ")
+        idade = int(input("Digite a idade do usuário: "))
+        adicionar_usuario(nome, idade)
+        print("Usuário adicionado com Sucesso!")
+    elif escolha == '2':
+        print("\nTodos os Usuários: ")
+        listar_usuarios()
+    elif escolha == '3':
+        id = int(input("Digite o ID do usuário a ser atualizado: "))
+        nome = input("Digite o novo nome do usuário: ")
+        idade = int(input("Digite a nova idade do usuário: "))
+        atualizar_usuario(id, nome, idade)
+        print("Usuário atualizado com sucesso!")
+    elif escolha == '4':
+        id = int(input("Digite o ID do usuário a ser deletado: "))
+        deletar_usuario(id)
